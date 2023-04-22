90641007a0419a53be63ee6214ad4f79b205431f
Mohamed Mounir <m.mounir.f@gmail.com>
2023-04-22 08:11:04 +0200

    listen to different localhost

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ server.register(proxy, {
   cacheURLs: 0,
 });
 
-server.listen({ port: 3000 }, (err, address) => {
+server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
