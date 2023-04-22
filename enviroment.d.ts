0efc48a926e843391f1f5cfd9013f0181ee5ab9b
Mohamed Mounir <m.mounir.f@gmail.com>
2023-04-22 09:33:10 +0200

    fix  railway build

---
+++ enviroment.d.ts
@@ -0,0 +1,6 @@
+export namespace NodeJS {
+  interface ProcessEnv {
+    UPSTREAM: string;
+    PORT?: number;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import proxy from '@fastify/http-proxy';
+import { config } from 'dotenv';
 import fastify from 'fastify';
 
 const server = fastify();
 
+config();
+
 server.register(proxy, {
-  upstream: 'https://api.newspaper.cloudai.network',
+  upstream: process.env.UPSTREAM,
   disableCache: true,
   cacheURLs: 0,
 });
 
-server.listen({ host: '0.0.0.0', port: 3000 }, (err, address) => {
+server.listen({ host: '0.0.0.0', port: Number(process.env.PORT) }, (err, address) => {
   if (err) {
     console.error(err);
     process.exit(1);
